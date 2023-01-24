1bd73c1f2a87d9c8dde94193dd1473a73dc2583b
Alex Urumov <alex.urumov@gmail.com>
2023-01-24 11:27:07 +0200

    Installed HTTP-Errors; Removed Test Layer

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,4 @@
-import express from 'express';
-import {router as testRouter} from './test.router'
+import express, {NextFunction, Request,  Response} from 'express';
 import {router as userRouter} from './user.router'
 import {router as meetingRouter} from './meeting.router'
 import {auth} from "../middlewares/auth.middleware";
@@ -7,6 +6,12 @@ import {auth} from "../middlewares/auth.middleware";
 export const routes = express.Router();
 
 routes.use(auth);
-routes.use('/test', testRouter);
 routes.use('/user', userRouter);
 routes.use('/meeting', meetingRouter);
+routes.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    if (err) {
+        console.error(err);
+        res.status(500).json('Oops! Something happened...');
+    }
+    next();
+});
