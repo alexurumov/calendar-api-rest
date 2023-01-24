61c41f21d67e065892fef497ae1d3f6e681bdefd
Alex Urumov <alex.urumov@gmail.com>
2023-01-24 15:35:21 +0200

    Extract Error Handler in separate middleware

---
+++ src/middlewares/error-handler.middleware.ts
@@ -0,0 +1,14 @@
+import {NextFunction, Request, Response} from "express";
+import {HttpError} from "http-errors";
+
+export function handleErrors(err: Error, req: Request, res: Response, next: NextFunction): Response | void {
+    if (err) {
+        console.error(err);
+
+        if (err instanceof HttpError) {
+            return res.status(err.status).json(err.message);
+        }
+        return res.status(500).json('Oops! Something happened...');
+    }
+    return next();
+}
--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,7 @@ import {router as userRouter} from './user.router'
 import {router as meetingRouter} from './meeting.router'
 import {auth} from "../middlewares/auth.middleware";
 import {HttpError} from "http-errors";
+import {handleErrors} from "../middlewares/error-handler.middleware";
 
 export const routes = express.Router();
 
@@ -10,16 +11,5 @@ routes.use(auth);
 routes.use('/user', userRouter);
 routes.use('/meeting', meetingRouter);
 
-// TODO: Export to function for enhanced readability
 // Error Handler
-routes.use((err: Error, req: Request, res: Response, next: NextFunction): Response | void => {
-    if (err) {
-        console.error(err);
-
-        if (err instanceof HttpError) {
-            return res.status(err.status).json(err.message);
-        }
-        return res.status(500).json('Oops! Something happened...');
-    }
-    next();
-});
+routes.use(handleErrors);
